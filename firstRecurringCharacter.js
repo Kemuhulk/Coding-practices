1a6f6a9e7aa7ff82831e424b51715fc615fe172f
Hema <118927072+Kemuhulk@users.noreply.github.com>
2025-01-06 18:37:45 +0530

    Add files via upload

---
+++ firstRecurringCharacter.js
@@ -0,0 +1,26 @@
+//Given an array = [2,5,1,2,5,4,6]
+//It should return the first recurring number or character.
+//Here output should be 2
+
+function firstRecurringCharacter1(input) {
+  for (let i = 0; i < input.length; i++) {
+    for (let j = i + 1; j < input.length; j++) {
+      if (input[i] === input[j]) {
+        return input[i];
+      }
+    }
+  }
+  return undefined;
+} //O(n^2)
+
+function firstRecurringCharacter2(input) {
+  let map = {};
+  for (let i = 0; i < input.length; i++) {
+    if (map[input[i]] !== undefined) {
+      return input[i];
+    } else {
+      map[input[i]] = i;
+    }
+  }
+  return undefined;
+} //O(n)
