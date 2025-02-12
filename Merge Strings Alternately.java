f5098d77389708aca9324295ddc3fee5f0f43123
Hema <118927072+Kemuhulk@users.noreply.github.com>
2025-02-12 16:36:33 +0530

    Create Merge Strings Alternately.java

---
+++ Merge Strings Alternately.java
@@ -0,0 +1,16 @@
+// Leetcode question no.1768 : Merge Strings Alternately
+
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder merged = new StringBuilder();
+        int i = 0, len1 = word1.length(), len2 = word2.length();
+
+        while (i < len1 || i < len2) {
+            if (i < len1) merged.append(word1.charAt(i)); // Add from word1
+            if (i < len2) merged.append(word2.charAt(i)); // Add from word2
+            i++;
+        }
+        
+        return merged.toString();
+    }
+}
