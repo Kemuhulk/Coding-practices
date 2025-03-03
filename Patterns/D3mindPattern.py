d9ebe67c3db22ef3f6e734d440cc5bda1d695454
Hema <118927072+Kemuhulk@users.noreply.github.com>
2025-03-03 20:45:41 +0530

    Update D3mindPattern.py

--- Patterns/D3mindPattern.py
+++ Patterns/D3mindPattern.py
@@ -1,4 +1,12 @@
-n = int(input())
+# Enter a number:5
+
+#         1
+#       2 1 2
+#     3 2 1 2 3
+#   4 3 2 1 2 3 4
+# 5 4 3 2 1 2 3 4 5
+
+n = int(input("Enter a number:"))
 for i in range(1, n+1):
     print(" "*(n-i),end="")
     for j in range(i,0,-1):
